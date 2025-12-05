3d9ab479c9e2d560bdce75a4729fb01033c9547c
mdk <mdk@lostindetails.com>
2025-12-05 07:52:40 +0100

    wasm +_-

--- src/wasm.zig
+++ src/wasm.zig
@@ -3,6 +3,7 @@ const day1 = @import("day01.zig");
 const day2 = @import("day02.zig");
 const day3 = @import("day03.zig");
 const day4 = @import("day04.zig");
+const day5 = @import("day05.zig");
 
 export fn day1_part1() i64 {
   return day1.part1() catch -1;
@@ -27,4 +28,7 @@ export fn day4_part1() i64 {
 }
 export fn day4_part2() i64 {
   return day4.part2(std.heap.wasm_allocator) catch -1;
+}
+export fn day5_part1() u64 {
+  return day5.part1(std.heap.wasm_allocator) catch 0;
 }
